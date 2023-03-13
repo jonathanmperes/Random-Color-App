ab06ab14046b31832022df4a2afbbd9e78b6d05d
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-03-13 09:40:55 -0300

    extension ColorsTableViewController set

--- RandomColorApp/ColorsTableViewController.swift
+++ RandomColorApp/ColorsTableViewController.swift
@@ -14,3 +14,14 @@ class ColorsTableViewController: UIViewController {
 
     }
 }
+
+extension ColorsTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
